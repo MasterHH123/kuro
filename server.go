74fef1bb9d265f28e60d85c4dbf04508e5aa5ca5
Horacio H <hernandezh4@hotmail.com>
2024-12-30 19:27:44 -0800

    Added htmx to the index page, will continue to work on hospital validation when adding a doctor.

--- server.go
+++ server.go
@@ -3,16 +3,18 @@ package main
 import (
 	"net/http"
 
+	"github.com/gin-gonic/gin"
+
 	"kuro/app/controllers"
 	"kuro/db"
-
-	"github.com/gin-gonic/gin"
 )
 
 func main(){
     router := gin.Default()
+    router.LoadHTMLGlob("client/*.html")
+
     router.GET("/", index)
-    router.Static("/client", "./client")
+    router.GET("/HTMXTest", HTMXTestRoute)
     router.GET("/db_test", db.DBTestHandler)
     router.POST("/create_doctor", controllers.CreateDoctor)
 
@@ -21,8 +23,11 @@ func main(){
 }
 
 func index (c *gin.Context){
-    response := gin.H{"message": "Welcome to kuro's first route!!!"}
-    c.IndentedJSON(http.StatusOK, response)
+    response := gin.H{"message": "kuro's index page"}
+    c.HTML(http.StatusOK, "index.html", response)
 }
 
-
+func HTMXTestRoute (c *gin.Context)  {
+    response := "Welcome to kuro!!! We're using htmx to build this website!!!!"
+    c.IndentedJSON(http.StatusOK, response)
+}
